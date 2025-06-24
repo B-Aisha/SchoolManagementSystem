eee59d0d71bc62bcfc0020245fbffcf48647e5c6
B-Aisha <aishabakari918@gmail.com>
2025-06-24 16:34:04 +0300

    Backend logic for updating the course list

--- backend/SchoolManagementAPI/obj/Debug/net9.0/SchoolManagementAPI.AssemblyInfo.cs
+++ backend/SchoolManagementAPI/obj/Debug/net9.0/SchoolManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b31f70ea2041f0dfc66be7a9cb2bfa6bc41887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0686c8d2118ec9757d67b1993e1205f7c31f06af")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
